b74ac664c3881289ad71857318c556af4489b853
Jose Flores <20221043@uthh.edu.mx>
2024-10-25 15:23:28 -0600

    permitiendo 2

--- config/db.js
+++ config/db.js
@@ -1,28 +1,19 @@
-// db.js
 require('dotenv').config();
 const mysql = require('mysql');
 
-// Crear un pool de conexiones
-const connection = mysql.createPool({
-    connectionLimit: 12, // Número máximo de conexiones en el pool
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    acquireTimeout: 30000, // Tiempo de espera en milisegundos para obtener una conexión
-    waitForConnections: true, // Esperar si no hay conexiones disponibles
-    queueLimit: 0,
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-// Manejo de errores en el pool
-pool.on('connection', (connection) => {
-    console.log('Conexión a la base de datos establecida.');
+connection.connect((err) => {
+  if (err) {
+    console.error('Error conectando a la base de datos:', err);
+    return;
+  }
+  console.log('Conectado a la base de datos MySQL');
 });
-
-pool.on('error', (err) => {
-    console.error('Error en la conexión del pool:', err);
-});
-
-// Exportar el pool para que se use en otras partes de la aplicación
 
 module.exports = connection;
